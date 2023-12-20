b9f2aba753a2f396e50de020275f0dd252fbb557
jlenon7 <lenonSec7@gmail.com>
2023-12-20 23:57:10 +0000

    chore(npm): update dependencies

--- bin/main.ts
+++ bin/main.ts
@@ -8,9 +8,31 @@
  */
 
 import Benchmark from '#src/benchmark'
+import { parseArgs } from 'node:util'
 
-await Benchmark.adonisjs()
-await Benchmark.athenna()
-await Benchmark.express()
-await Benchmark.fastify()
-await Benchmark.nestjs()
+const { values } = parseArgs({
+  options: {
+    framework: {
+      type: 'string',
+      short: 'f',
+      multiple: true
+    }
+  },
+  args: process.argv.slice(2)
+})
+
+if (!values.framework) {
+  await Benchmark.adonisjs()
+  await Benchmark.athenna()
+  await Benchmark.express()
+  await Benchmark.fastify()
+  await Benchmark.nestjs()
+}
+
+if (values.framework.length === 1 && values.framework.includes(',')) {
+  values.framework = values.framework[0].split(',')
+}
+
+for (const framework of values.framework) {
+  await Benchmark[framework]()
+}
--- framework/athenna/src/routes/http.js
+++ framework/athenna/src/routes/http.js
@@ -1,4 +1,4 @@
 import { Route } from '@athenna/http'
-Route.get('/', async ctx => {
+Route.get('/', ctx => {
   return ctx.response.status(200).send({ status: 'ok' })
 })
