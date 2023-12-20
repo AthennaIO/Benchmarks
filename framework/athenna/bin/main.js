fab386beb728bb41f9e07237e5f6db4ed1933cfb
jlenon7 <lenonSec7@gmail.com>
2023-12-20 16:23:41 +0000

    chore(npm): update dependencies

--- framework/athenna/bin/main.js
+++ framework/athenna/bin/main.js
@@ -1,9 +1,17 @@
 import { Ignite } from '@athenna/core'
+import { Config } from '@athenna/config'
 const ignite = await new Ignite().load(import.meta.url, {
+  beforePath: '/',
   bootLogs: false,
   shutdownLogs: false,
 })
-await ignite.httpServer({
+Config.set('http', {
   port: 3000,
   host: 'localhost',
+  cors: { enabled: false },
+  logger: { enabled: false },
+  rTracer: { enabled: false },
+  swagger: { enabled: false },
+  rateLimit: { enabled: false },
 })
+await ignite.httpServer()
